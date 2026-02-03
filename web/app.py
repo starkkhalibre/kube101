8d8669fac79b21c79bdf8051a8e039f2cadd8f82
Bothkhalibre <vannaboth.meng@khalibre.com>
2026-02-03 08:47:13 +0700

    update code

--- web/app.py
+++ web/app.py
@@ -8,7 +8,7 @@ redis = Redis(host=os.environ.get('REDIS_HOST', 'redis'), port=6379)
 @app.route('/')
 def hello():
     redis.incr('hits')
-    return 'Hello Container World from %s! I have been seen %s times.\n' %  ( platform.node(), redis.get('hits') )
+    return 'Hello New Container World from %s! I have been seen %s times.\n' %  ( platform.node(), redis.get('hits'))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
